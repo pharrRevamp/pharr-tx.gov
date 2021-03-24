a7a3793f4cd02c10ef1ea9588ac6cfd80cfa2b37
Michael Trevino <michaeltrevino@PharrITs-iMac.local>
2021-03-24 10:01:55 -0500

    homepage set up to go

--- pharr-tx_gov/src/App.js
+++ pharr-tx_gov/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import HomePage from "./homePage/HomePage";
 import "./css/index.css";
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
