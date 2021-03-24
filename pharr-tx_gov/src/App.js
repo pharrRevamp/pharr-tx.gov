9a7dd936d0a2689e86226ab96ed8bb5188d12b9a
Michael Trevino <michaeltrevino@PharrITs-iMac.local>
2021-03-24 10:55:05 -0500

    working on the navbar1

--- pharr-tx_gov/src/App.js
+++ pharr-tx_gov/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import HomePage from "./homePage/HomePage";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import "./css/index.css";
+
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Hello World! testing!</h1>
       <HomePage />
+      <Footer />
     </div>
   );
 }
---
+++ pharr-tx_gov/src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <header>
+        <h1>Image Here</h1>
+        <nav>
+          <a href="#.">About</a>
+          <a href="#.">Business</a>
+          <a href="#.">Department</a>
+          <a href="#.">Public Safety</a>
+          <a href="#.">Quick Access</a>
+        </nav>
+      </header>
+    </div>
+  );
+}
+export default NavBar;
